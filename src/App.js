bd551eec6d8d4425ab62ec5a5922054e498b25b4
danielngira <danielngira7@gmail.com>
2024-07-09 22:44:12 -0700

    Made board

--- src/App.js
+++ src/App.js
@@ -1,3 +1,40 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from 'react';
+function Square() {
+    const [value, setValue] = useState(null);
+
+    function handleClick() {
+      console.log('Button clicked');
+    }
+  return (
+   <button 
+      className="square"
+      onClick={handleClick}
+   >
+      {value}
+  </button>
+ );
 }
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value={1} />
+        <Square value={2} />
+        <Square value={3} />
+      </div>
+      <div className="board-row">
+        <Square value={4} />
+        <Square value={5} />
+        <Square value={6} />
+      </div>
+      <div className="board-row">
+        <Square value={7} />
+        <Square value={8} />
+        <Square value={9} />
+      </div>
+    </>
+  );
+}
+
+
